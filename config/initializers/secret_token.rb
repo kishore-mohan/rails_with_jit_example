c96b42e823306de8eaec2fa1944beab92c4ddb3a
kishore-mohan <kishorevlrtn@gmail.com>
2013-12-10 17:14:32 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CBHackathon::Application.config.secret_token = 'b3230dcfb5974f01554104dcdde8ea36bf7060ae528c620593a3aa4f2719b62e439abc1425df271be6afec82455b953d0fc461d7cb0a317036e0c9516e70257c'
